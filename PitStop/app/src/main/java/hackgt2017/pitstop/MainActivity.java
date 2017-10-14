ee31d131f104d7268368d7a4e259e1522f89f0c7
Dorkly <bree_cakesx3@live.com>
2017-10-14 11:30:26 -0400

    FINALLY

---
+++ PitStop/app/src/main/java/hackgt2017/pitstop/MainActivity.java
@@ -0,0 +1,91 @@
+package hackgt2017.pitstop;
+
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.mashape.unirest.http.*;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import com.mashape.unirest.request.HttpRequest;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public void sendMessage(View view) {
+
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+
+        TextView txtView = (TextView) findViewById(R.id.textView1);
+        txtView.setText(message);
+
+        new CallMashapeAsync().execute(message);
+    }
+
+    private class CallMashapeAsync extends AsyncTask<String, Integer, HttpResponse<String>> {
+
+        protected HttpResponse<String> doInBackground(String... msg) {
+
+            HttpResponse<String> response = null;
+            try {
+                response = Unirest.get("https://hackgt-api.ncrcloud.com/catalog/2.0.2/items/1.0/snapshot")
+                        .header("authorization", "AccessToken eyJhbGciOiJFUzI1NiJ9.eyJtdGgiOlsicGFzc3dvcmQiXSwic3ViIjoiYWNjdDpvcmctMUBhZG1pbiIsIm5iZiI6MTUwNzk5MjI2OCwib3JnIjoiL29yZy0xLyIsImlzcyI6Ik5DUiIsInJscyI6ImVKeUZVMEZ5Z3pBTVBNTnIyaWNvUmlGcXdmTElJcG4wL3crcDdHQVNFeUFuc0x5NzNsbEpYNjNIMERiVW9WZlNlOXMwVjhJYml2MUFONUtucUFMS2RqWUFTaENLYUhlOWdGZjdya0g3OU9WaVpFOVdJZDliOFlJdzZPVlF5TEUvVThKR1ZEVldQRVFIRm9YQktpdzllUG9ESmZhVnJ6ZHVaZWlKQzhKWGlzWitYTVRwRkoxUStLd0h6bUhNTG44eEJicmg5c2xXRHVRcXZTbHV5ejRSU3lKVkpFZXZKR0NzUzFVN204a2krS2hTQlNVOFlPV3F3eXVIckJFRE9pM2hXaFRRUHlnL1BJbUhZUlYwVlRWWGtqUFRlOEJOUDd2emRSUFNkQUVoRE9SSzMxKzhyQ1ppVCtqVjhXN1QzOXMyUTAvcDBHRVkrRDVpWHBHQ2dFQWplT2h4Vlg5MGM1TGlhNzEySTlDUXA5OTNDWjV5TDl1M2dzNHRtWVVEV2pNY3VBc1dwRzBIaVBxTjZlN1I1TW91dWlrcWp4c29GRW5QWEptNnRqbFRhdG9mNXNETFBHZzN0dVd0eVRJQUE4NTI1RHU1c2Qzay9pRDhMTEJRaTlRQ1o4a1o3TkQvQVVxb0ZKOD0iLCJleHAiOjE1MDgwMjEwNjgsImlhdCI6MTUwNzk5MjI2OCwianRpIjoiYTc4MDAxMmEtNzRiOC00NWMyLWFjM2QtNzY4NDgzOGZmNTcxIn0.MEQCIDDjUuVPVdMYd2hWLLuh7KiaLn4c-bGJQ2GNlGAPZzeFAiAHhw91imAxc41LTUTb-aNhpW1JCAq-vA2JTBpm1qzpvQ")
+                        .header("cache-control", "no-cache")
+                        .header("nep-application-key", "8a82859f5ef21870015ef2fa5e5f0000")
+                        .header("accept", "application/json, text/plain, */*")
+                        .header("content-type", "application/json")
+                        .header("postman-token", "6dfcb64c-c5b4-f085-32fe-0f2db9c68c24")
+                        .asString();
+                Log.v(response.getBody().toString(),"AAAAAA");
+                Log.i(response.getBody().toString(),"gfdf");
+                Log.d("cccc",String.valueOf(response));
+                Log.e("bb",response.getBody().toString());
+                Log.v("aaa",response.getBody().toString());
+            } catch (UnirestException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            return response;
+        }
+
+        protected void onProgressUpdate(Integer...integers) {
+        }
+
+        protected void onPostExecute(HttpResponse<JsonNode> response) {
+            Log.d("aaaaaaaaaaa","jkjkj");
+            String answer = response.getBody().toString();
+            TextView txtView = (TextView) findViewById(R.id.textView1);
+            txtView.setText(answer);
+            Log.v(response.getBody().toString(),"AAAAAA");
+            Log.i(response.getBody().toString(),"fjxfhxdkjhjfkfzdjh");
+            Log.d("fjxfhxdkjhjfkfzdjh",String.valueOf(answer));
+            Log.e("fjxfhxdkjhjfkfzdjh",answer);
+            Log.w("fjxfhxdkjhjfkfzdjh",answer);
+            Log.v("fjxfhxdkjhjfkfzdjh",answer);
+
+        }
+    }
+}
+
+
+
+
--- PitStop/app/src/main/java/hackgt2017/pitstop/httpconnectiontest.java
+++ PitStop/app/src/main/java/hackgt2017/pitstop/httpconnectiontest.java
@@ -1,5 +1,12 @@
 package hackgt2017.pitstop;
 
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
@@ -9,15 +16,39 @@ import com.mashape.unirest.http.exceptions.UnirestException;
  * Created by John on 2017-10-13.
  */
 
-public class httpconnectiontest {
+public class httpconnectiontest extends Activity{
 
-    HttpResponse<JsonNode> jsonResponse = Unirest.post("https://hackgt-api.ncrcloud.com/order/orders")
-            .header("accept", "application/json")
-            .queryString("apiKey", "8a82859f5ef21870015ef2fa5e5f0000")
-            .field("parameter", "value")
-            .field("foo", "bar")
-            .asJson();
+    public void sendMessage(View view) {
 
-    public httpconnectiontest() throws UnirestException {
+        new CallMashapeAsync().execute("hellloooope");
+    }
+
+    private class CallMashapeAsync extends AsyncTask<String, Integer, HttpResponse<JsonNode>> {
+
+        protected HttpResponse<JsonNode> doInBackground(String... msg) {
+
+            HttpResponse<JsonNode> request = null;
+            try {
+                request = Unirest.get("https://webknox-question-answering.p.mashape.com/questions/answers?question=" + msg[0] + "&answerLookup=true&answerSearch=true")
+                        .header("X-Mashape-Authorization", "<Insert your Mashape key here>")
+                        .asJson();
+            } catch (UnirestException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            return request;
+        }
+
+        protected void onProgressUpdate(Integer...integers) {
+        }
+
+        protected void onPostExecute(HttpResponse<JsonNode> response) {
+            String answer = response.getBody().toString();
+           Log.d("data",answer);
+            Log.v("data",answer);
+            Log.i("data",answer);
+            Log.e("data",answer);
+        }
     }
 }
