1c657a50ce7b14887a7e062cec637f744918048d
AustinJ's Mac <AustinJ@lawn-128-61-22-209.lawn.gatech.edu>
2017-10-15 02:37:41 -0400

    new screen

---
+++ PitStop/app/src/main/java/hackgt2017/pitstop/orderListActivity.java
@@ -0,0 +1,33 @@
+package hackgt2017.pitstop;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by AustinJ on 10/15/17.
+ */
+
+public class orderListActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button ReturnButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_list);
+
+        ReturnButton = (Button) findViewById(R.id.ReturnButton);
+        ReturnButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        // Perform action on click
+        if(v == ReturnButton) {
+            startActivity(new Intent(getApplicationContext(), UserMainActivity.class));
+        }
+    }
+}
---
+++ PitStop/app/src/main/java/hackgt2017/pitstop/profileActivity.java
@@ -0,0 +1,15 @@
+package hackgt2017.pitstop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by AustinJ on 10/15/17.
+ */
+
+public class profileActivity extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    public void onClick(View v) {
+
+    }
+}
